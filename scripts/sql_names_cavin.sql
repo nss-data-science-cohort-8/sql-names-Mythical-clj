28018efbb7990b70ba45b212e4f321ce96c9a571
Mythical-clj <cavinjacobson@gmail.com>
2025-02-22 13:01:33 -0500

    Answered all but 20. 2/22

--- scripts/sql_names_cavin.sql
+++ scripts/sql_names_cavin.sql
@@ -96,43 +96,88 @@ GROUP BY year
 ORDER BY name_count DESC
 LIMIT 1;
 
-
-
 -- 11. What is the most popular name for a girl that starts with the letter X?
 
+SELECT name, gender, SUM(num_registered) AS total
+FROM names
+WHERE name LIKE 'X%'
+	AND gender = 'F'
+GROUP BY name, gender
+ORDER BY total DESC
+LIMIT 1;
+
+-- Ximena
 
 -- 12. Write a query to find all (distinct) names that start with a 'Q' but whose second letter is not 'u'
 
+SELECT DISTINCT name, gender, SUM(num_registered) AS total
+FROM names
+WHERE name LIKE 'Q%'
+	AND name NOT LIKE '_u%'
+GROUP BY name, gender
+ORDER BY total DESC;
 
 -- 13. Which is the more popular spelling between "Stephen" and "Steven"? 
--- Use a single query to answer this question.
+-- Use a single query to answer this question. 
 
+SELECT name, SUM(num_registered) AS total
+FROM names
+WHERE name LIKE 'Ste%en'
+GROUP BY name
+ORDER BY total DESC
+LIMIT 2;
+
+-- Steven is the more popular spelling
 
 -- 14. Find all names that are "unisex" - 
 -- that is all names that have been used both for boys and for girls.
 
+SELECT name
+FROM names
+GROUP BY name
+HAVING COUNT(DISTINCT gender) = 2  
+ORDER BY name
+LIMIT 5;
 
 -- 15. Find all names that have made an appearance in every single year since 1880.
 
-
+SELECT name
+FROM names
+WHERE year >=1880
+GROUP BY name
+HAVING COUNT(DISTINCT(year)) = (2019 - 1880)
+LIMIT 5;
 
 -- 16. Find all names that have only appeared in one year.
 
-
-
+SELECT name, COUNT(name)
+FROM names
+GROUP BY name
+HAVING COUNT(year) = 1;
 
 -- 17. Find all names that only appeared in the 1950s.
 
-
-
+SELECT name
+FROM names
+GROUP BY name
+HAVING MIN(year) >= 1950 
+	AND MAX(year) <= 1959;
 
 -- 18. Find all names that made their first appearance in the 2010s.
 
-
+SELECT name
+FROM names
+GROUP BY name
+HAVING MIN(year)>=2010 
+	AND MIN(year) < 2020;
 
 -- 19. Find the names that have not been used in the longest.
 
-
+SELECT name, MAX(year) AS oldest
+FROM names
+GROUP BY name
+ORDER BY oldest
+LIMIT 5;
 
 
 -- 20. Come up with a question that you would like to answer using this dataset. 
